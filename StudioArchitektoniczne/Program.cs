8c7ab15183a79000c377da42c63c89dd3d74fcd2
Mateusz Buchajewicz <theherrlupardo@gmail.com>
2019-10-25 10:53:11 +0200

    stucture refactoring

--- StudioArchitektoniczne/Program.cs
+++ StudioArchitektoniczne/Program.cs
@@ -6,7 +6,7 @@ namespace StudioArchitektoniczne
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello world");
         }
     }
 }
